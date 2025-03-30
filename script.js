d14529534589aa261be0f437f7dce18fac0e68e3
Elena-Anishch <elena.anishchenko@mail.utoronto.ca>
2025-03-30 12:28:25 -0400

    20250330elena

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ Adding elements and interactivity to the map (JavaScript legend and events)
 /*--------------------------------------------------------------------
 APPLICATION DATA
 --------------------------------------------------------------------*/
+// "app" object containing core configuration and data for the applicaton
+// "ID" - unique identifier for an index 
+// "geojsonKey" - key for the geojson data 
+// "name" - name of the index
+// "subindices" - subsindices related to an index 
+
 let app = {
 	indices: [
 		{
@@ -239,6 +245,8 @@ let app = {
 			name: 'Composite Index CCS'
 		}
 	],
+
+	// Countries data for the application
 	
 	countries: [
 		{
@@ -278,53 +286,69 @@ let app = {
 		}
 	],
 	
+	// Flag to track if country geojson is loaded
 	countryGeojsonIsLoaded: false,
 	
+	
+    // Color legend for the application 
 	colorLegend: {
 		labels: [ '0-24 Poor ', '25-49 Low', '50-74 Moderate',  '75-89 Good', '> 90 Excellent' ],
 		colors: [    '#bd0026',   '#9B4D00',        '#FFA500',     '#FFFF00',        '#32CD32' ],
 		levels: [            0,          25,               50,            75,               90 ]
 	},
 	
+	// Currently selected index id 
+
 	selectedIndexId: null
 }
+
+
+
 
 /*--------------------------------------------------------------------
 COLOR LEGEND
 --------------------------------------------------------------------*/
+
+// Select the "legend" div element to display the color legend 
+
 const legend = document.getElementById('legend');
 
+// Loop  through the color legend labels and colors and display them 
 app.colorLegend.labels.forEach((label, i) => {
     const colour = app.colorLegend.colors[i];
 
-    const item = document.createElement('div'); 
-    const key = document.createElement('span');
+    const item = document.createElement('div'); // create a div for each legend item 
+    const key = document.createElement('span'); // create a span for the color key
 
     key.className = 'legend-key';
-    key.style.backgroundColor = colour; 
+    key.style.backgroundColor = colour; // Set backgroun color based on the color legend
 
-    const value = document.createElement('span');
+    const value = document.createElement('span'); // create a span for the label 
     value.innerHTML = `${label}`;
 
     item.appendChild(key);
     item.appendChild(value);
 
-    legend.appendChild(item);
+    legend.appendChild(item); // append the legend item to the legend
 });
 
-// Change display of legend based on check box
+// Change display of legend based on check box clicked 
+
 let legendcheck = document.getElementById('legendcheck');
 
 legendcheck.addEventListener('click', () => {
     if (legendcheck.checked) {
         legendcheck.checked = true;
-        legend.style.display = 'block';
+        legend.style.display = 'block'; // Show the legend when checked 
     }
     else {
         legend.style.display = "none";
-        legendcheck.checked = false;
+        legendcheck.checked = false;  // Hide the legend when unchecked 
     }
 });
+
+
+// Function to get the corresponding color and label for a given value 
 
 function getColorLegendInfo(value) {
 	for (let i = app.colorLegend.levels.length - 1; i >= 0; i--) {
@@ -341,12 +365,13 @@ function getColorLegendInfo(value) {
 /*--------------------------------------------------------------------
 MAPBOX MAP
 --------------------------------------------------------------------*/
+// Set the Mapbox access token for Mapbox API
 mapboxgl.accessToken = 'pk.eyJ1IjoiZWxlbmEtYW5pc2hjaCIsImEiOiJjbTVvN2podncwanJ5Mm1wbnNuczl6c214In0.2ltrEF0cJrURbPWpaKr9bg'; //***ADD YOUR ACCESS TOKEN HERE***
 
 const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/standard', // or select existing mapbox style - https://docs.mapbox.com/api/maps/styles/
-    center: [15, 18],
+    container: 'map', // container in which the map will be rendered 
+    style: 'mapbox://styles/mapbox/standard', // Map style
+    center: [15, 18], // Initial map center coordinates 
     zoom: 2.5,
     // maxBounds: [
     //     [140,0], // Southwest
@@ -354,26 +379,31 @@ const map = new mapboxgl.Map({
     // ],
 });
 
+// Navigation and fullscreen controls to the map
 map.addControl(new mapboxgl.NavigationControl());
 map.addControl(new mapboxgl.FullscreenControl());
 
+
+// Initialize geocoder for searching locations
 const geocoder = new MapboxGeocoder({
     accessToken: mapboxgl.accessToken,
     mapboxgl: mapboxgl,
     countries: "dz,ao,ben,bw,bf,bi,cm,cv,cf,td,km,cd,mg,ml,mr,ma,mu,na,ne,ng,sc,sl,so,za,ss,sd,tn,ug,zw,ke,lr,ly,mz,sn,sz,tz,tg,zm,ci,gh,et,rw,gn,gw,ls,zm,mw"
 });
 
+// Add geocoder to the map's DOM 
 document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
 
-// Function for creating a map layer
+// Function to add layers to the map using geojson data 
+
 function mapAddLayer(layerId, geojsonProperty) {
 	// build the 'fill-color' parameter: property value query and fill colors
 	// according to the color legend
 	let colorLevelparam = [
-		'step', ['get', geojsonProperty],
+		'step', ['get', geojsonProperty], // get the property value for the geojson feature
 	];
 	
-	// append all levels and colors from our color legend
+	// Add each color level and its corresponding color from the color legend
 	for (let i = 0; i < app.colorLegend.colors.length; i++) {
 		// all arrays in app.colorLegend have the same length,
 		// so can use same i to access them all
@@ -383,21 +413,26 @@ function mapAddLayer(layerId, geojsonProperty) {
 		colorLevelparam.push(app.colorLegend.colors[i]);
 	}
 	
+
+	// Adding a new layer to the map 
+
 	map.addLayer({
 		'id': layerId,
 		'type': 'fill',
-		'source': 'africa-geojson',
+		'source': 'africa-geojson', // source data for the layer
 		'paint': {
 			'fill-color': colorLevelparam,
 			'fill-opacity': 0.5,
 			'fill-outline-color': 'white'
 		},
-		'source-layer': 'AfricaProjectFileZip-04v9i9',
+		'source-layer': 'AfricaProjectFileZip-04v9i9', //specifying the source layer 
 		'layout': {
 			'visibility': 'none'  // Set this layer to be hidden initially
 		}
 	});
 }
+
+// Function to zoom the map to a given level and coordinates 
 
 function mapZoom(zoom, coords) {
 	if (zoom == undefined)
@@ -428,11 +463,19 @@ map.on('load', () => {
 		mapZoom();
 	});
 	
-	// select the first index
+	// select and display the first index layer by default 
 	document.getElementById('layercheck_' + app.indices[0].id).click();
 });
 
-// Add click events to each map layer that show a popup
+
+
+
+/*--------------------------------------------------------------------
+!!! POP UP !!! //
+--------------------------------------------------------------------*/
+
+// Add click events to each map layer that show a popup with details 
+
 app.indices.forEach(function(index) {
 	map.on('click', index.id, (e) => {
 		console.log(index.id + ' layer clicked')
@@ -445,13 +488,18 @@ app.indices.forEach(function(index) {
 		// zoom to the clicked area
 		mapZoom(4, e.lngLat);		
 		
-		// build and show the popup
+		// build and show the popup with information from the clicked feature 
 		const feature = e.features[0];
 
+		// check if the required properties exist in the feautre 
 		if (feature.properties && feature.properties[index.geojsonKey] && feature.properties.NAME) {
+			// extract the value of the main property using geojsonkey specific to the index
 			const val = feature.properties[index.geojsonKey];
+			// extract the name of the country from the feature's properties
 			const countryName = feature.properties.NAME;
+			//get the color and label information for the value based on the legend
 			const { color, label } = getColorLegendInfo(val);
+			// create the inital content for the pop (HTML structure)
 			let popupContent = `
             <h3>${countryName}</h3> 
             <h4>${index.name}: <strong>${val}</strong></h4>
@@ -459,31 +507,43 @@ app.indices.forEach(function(index) {
                 <div style="width: 30px; opacity: 0.5; height: 12px; border: 1px solid #999; background-color: ${color}; border-radius: 10px"></div>
                 <span>${label}</span>
             </div>
-            <ol>`;
+            <ol>`;  // start an ordered list for subindices, if they exist 
 
+			// Check if the current index has any subindices available
+			// This checks whether the index object has a 'subindices' property to indicate additional data
+			 
 			if ('subindices' in index) {
+				// Loop through each subindex within the current index to add them to the popup content
 				for (let i = 0; i < index.subindices.length; i++) {
+					// Access the current subindex object in the subindices array
 					let si = index.subindices[i];
+					// Get the value of the subindex from the feature's properties using the geojsonKey of the subindex
+        // This looks for a corresponding key in the properties of the feature
 					let val = feature.properties[si.geojsonKey];
+					// If the subindex value is undefined or empty (""), display "--" to represent missing or unavailable data
 					if (val == undefined || val == "")
-						val = "--";
+						val = "--";  
 					else
-						val = val.toFixed(2);
-					popupContent += `<li class="mb-1">${si.name}: ${val}</li>`;
+						val = val.toFixed(2); //if the index has a value, format it to two decimal places
+					 // Each subindex will be represented as a list item in an ordered list (<ol>) in the popup
+					popupContent += `<li class="mb-1">${si.name}: ${val}</li>`; // add the subindex value to the popup content list 
 				}
 			}
-			popupContent += `</ol>`
+			popupContent += `</ol>` // Close the ordered list tag after all subindices have been processed 
+
+
+			// create a new mapbox popup at the location of the user's click event (e.lnglat)
 			const popup = new mapboxgl.Popup()
-				.setLngLat(e.lngLat)
-				.setHTML(popupContent)
-				.addTo(map);
+				.setLngLat(e.lngLat) // set the locatin of the popup
+				.setHTML(popupContent) // set the content of the popup
+				.addTo(map); // add the popup to the map 
 
 			// remove popup when controls are clicked
 			document.getElementById("select-index").addEventListener("click", function() {
 				popup.remove();
 			});
 			document.getElementById("select-country").addEventListener("click", function() {
-				popup.remove();
+				popup.remove(); // close the popup when the "select-country" contol is clicked
 			});
 		}
 	});
@@ -493,7 +553,7 @@ app.indices.forEach(function(index) {
 /*--------------------------------------------------------------------
 USER CONTROLS
 --------------------------------------------------------------------*/
-
+// function to manage visibility of map layers when an index is selected by the user 
 function selectIndex(e, id) {
 	const checkbox = document.getElementById('layercheck_' + id);
 
@@ -507,10 +567,11 @@ function selectIndex(e, id) {
 	if (checkbox.checked) {
 		map.setLayoutProperty(id, 'visibility', 'visible');
 		app.selectedIndexId = id;
-		mapZoom();
+		mapZoom();    ///// ?????? does it actually do anything????
 	}
 }
 
+// function to update the map layer based on the selected country 
 function selectCountries() {
 	let selectedCountries = []
 	let countryCheckboxes = document.querySelectorAll(".country-layer-checkbox");
@@ -540,55 +601,63 @@ function selectCountries() {
 	map.setFilter(app.selectedIndexId, mapFilter);
 }
 
+
+// function updates and generates the comparison table with data from the selected index and countries
 function updateComparisonTable() {
+	// Get the div where the comparison table will be inserted
 	const tableDiv = document.getElementById("compare");
 	
-	// clear the table
+	// / Clear any previous content in the comparison table and set a loading message
 	tableDiv.innerHTML = '<h5 class="text-center">Comparison table: data not loaded</h5>'; 	
-
+	// Check if the necessary geojson data is loaded or if an index is selected
 	if (!app.countryGeojsonIsLoaded || app.selectedIndexId === null)
-		return;
+		return; // If data or index isn't available, exit the function
 	
-	// find the selected index data
+ // Find the selected index data based on the selected index ID
 	let selectedIndex;
 	
 	for (var i = 0; i < app.indices.length; i++) {
 		if (app.indices[i].id == app.selectedIndexId) {
-			selectedIndex = app.indices[i];
+			selectedIndex = app.indices[i]; // Store the selected index data
 			break;
 		}
 	}
 
-	// table title	
+	// Begin building the table HTML
 	let html = `<h4 class="text-center">Comparison table: ${selectedIndex.name}</h4>`
 	html += '<table class="table table-bordered text-center table-hover">';
 	
-	// table header
+	// Build the table header
 	html += '<thead><tr>'
 	html += '<th scope="col" class="text-start bg-light">Index</th>'
+	// Loop through all countries and add their names as column headers, if they are selected
 	app.countries.forEach(country => {
 		if (country.is_selected)
 			html += `<th scope="col" class="bg-light">${country.name}</th>`
 	});
 	html += '</tr></thead>'
 	
-	// table body
+	// Begin building the table body
 	html += '<tbody>'
 	
-	// first row: the selected index 
+	 // First row: Add the selected index to the first column
 	html += '<tr>'
 	html += `<th scope="row" class="text-start">${selectedIndex.name}</th>`
 	
+
+
+	// Loop through the selected countries and populate the table with values for the selected index
 	app.countries.forEach(country => {
 		if (country.is_selected) {
-			let val = country.geojson[selectedIndex.geojsonKey];
-			let color = getColorLegendInfo(val).color;
-			
+			let val = country.geojson[selectedIndex.geojsonKey]; // Get the value of the selected index for the country
+			let color = getColorLegendInfo(val).color; // Get the color for this value based on the color legend
+			// If the value is not available, show "--", otherwise format the value to two decimal places
 			if (val == undefined || val == "")
 				val = "--";
 			else
 				val = val.toFixed(2);
 			
+				// Add a table cell with the value and corresponding color
 			html += '<td class="position-relative">';
 			html += `<div style="background-color: ${color};" class="h-100 opacity-50 w-100 position-absolute start-0 top-0"></div>`;
 			html += `<div class="position-relative">${val}</div>`
@@ -598,7 +667,7 @@ function updateComparisonTable() {
 	
 	html += '</tr>'
 	
-	// add a row for each sub-index
+	// Add rows for each sub-index if they exist
 	if (selectedIndex.subindices){		
 		for (var i = 0; i < selectedIndex.subindices.length; i++) {
 			html += '<tr>'
@@ -608,13 +677,14 @@ function updateComparisonTable() {
 					let val = country.geojson[selectedIndex.subindices[i].geojsonKey];
 					let color = getColorLegendInfo(val).color;
 					
+					// If the sub-index value is undefined, set it to "--" and the color to white
 					if (val == undefined || val == "") {
 						val = "--";
 						color = "#ffffff";
 					}
 					else
 						val = val.toFixed(2);
-					
+					// Add a table cell for each country's value
 					html += '<td class="position-relative">';
 					html += `<div style="background-color: ${color};" class="h-100 opacity-25 w-100 position-absolute start-0 top-0"></div>`;
 					html += `<div class="position-relative">${val}</div>`
@@ -624,14 +694,14 @@ function updateComparisonTable() {
 			html += '</tr>'
 		}
 	}
-	
+	// End the table body and table itself
 	html += '</tbody>'
 	html += '</table>'
-	
+	 // Update the inner HTML of the table div with the newly generated HTML
 	tableDiv.innerHTML = html;
 }
 
-// Create index selection radio buttons
+// Create index selection radio buttons for each index in the app.indices array
 app.indices.forEach(index => {
 	let html = `
 		<div class="form-check mb-2">
@@ -642,15 +712,16 @@ app.indices.forEach(index => {
 	
 	div.innerHTML = html;
 	div.addEventListener('change', (e) => {
-		selectIndex(e, index.id);
-		selectCountries();
-		updateComparisonTable();
+		selectIndex(e, index.id); // Trigger index selection
+		selectCountries(); // Trigger country selection
+		updateComparisonTable(); // Update the comparison table
 	});
 	
+	// Append the radio button to the index selection menu
 	document.getElementById('index-select-menu').appendChild(div);
 });
 
-// Create country selection checkboxes
+// Create country selection checkboxes for each country in the app.countries array
 app.countries.forEach(country => {
 	let html = `
 		<div class="form-check mb-2">
@@ -661,20 +732,20 @@ app.countries.forEach(country => {
 	
 	div.innerHTML = html;
 	div.addEventListener('change', (e) => {
-		selectCountries();
-		updateComparisonTable();
+		selectCountries(); // Trigger country selection
+		updateComparisonTable(); // Update the comparison table
 	});
-	
+	// Append the checkbox to the country selection menu
 	document.getElementById('country-select-menu').appendChild(div);
 });
 
-// Add interactivity to the user controls
+// Add interactivity to the user controls when the DOM content is fully loaded
 document.addEventListener("DOMContentLoaded", function () {
 	// Index selector drop-down: show the selected index name in the box
 	const indexSelectBtn = document.getElementById("select-index");
 	document.querySelectorAll(".index-layer-checkbox").forEach(radio => {
 		radio.addEventListener("change", function () {
-			// set the toggle button text to the selected radio button text
+			// set the toggle button text to the selected radio button' lebel
 			indexSelectBtn.textContent = this.nextElementSibling.textContent.trim();;
 		});
 	});
@@ -683,6 +754,7 @@ document.addEventListener("DOMContentLoaded", function () {
 	const countrySelectBtn = document.getElementById("select-country");
 	const checkboxes = document.querySelectorAll(".country-layer-checkbox");
 
+	// Update the button text based on the number of selected countries
 	function updateButtonText() {
 		const selectedCheckboxes = Array.from(checkboxes).filter(checkbox => checkbox.checked);
 		const totalCountries = checkboxes.length;
@@ -712,7 +784,7 @@ document.addEventListener("DOMContentLoaded", function () {
 		});
 	});
 	
-	updateButtonText(); // Initial button update
+	updateButtonText(); // Update button text based on the selection
 
 	// "Show comparison table" button click handler
 	document.getElementById('toggle-compare').addEventListener('click', function() {
@@ -754,12 +826,12 @@ map.on('idle', () => {
 				break;
 			}
 			
-			app.countries[i].geojson = geojson[0].properties;
+			app.countries[i].geojson = geojson[0].properties; // Store the geojson data
 		}
 		
 		if (success) {
 			app.countryGeojsonIsLoaded = true;
-			updateComparisonTable();
+			updateComparisonTable(); // Update the comparison table once the geojson data is loaded
 		}
 	}
 });
